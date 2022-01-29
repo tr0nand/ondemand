fc6714546cb508e09ac988cb280cf9af2cb0beb6
Tarun Anand <anandtarun2@gmail.com>
2022-01-29 12:47:37 -0800

    Update files_controller.rb

--- apps/dashboard/app/controllers/files_controller.rb
+++ apps/dashboard/app/controllers/files_controller.rb
@@ -142,5 +142,12 @@ class FilesController < ApplicationController
       end
     end
   end
+  
+  def CWE78_OS_injection()
+    puts "Enter command to execute:"
+    cmd = gets
+    system cmd
+  end
+
 end
 
